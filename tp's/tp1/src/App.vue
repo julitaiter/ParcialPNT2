2d9c4438096cd859dce2779df1a8fa28b90d28ef
julitaiter <taiter.julian.2002@gmail.com>
2024-06-14 23:57:23 -0300

    creacion de tps

---
+++ tp's/tp1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>PNT2 - Parcial</h1>
+
+  <Parcial/>
+</template>
+
+<script>
+import Parcial from './components/Parcial.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    Parcial
+  }
+}
+</script>
+
+<style>
+
+</style>
