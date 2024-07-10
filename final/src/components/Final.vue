fed4f100fb185d9303956163565928374c3ef162
julitaiter <taiter.julian.2002@gmail.com>
2024-07-10 20:49:41 -0300

    obtengo dolar blue

--- final/src/components/Final.vue
+++ final/src/components/Final.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
-        <div>
-            <label for="pesos">Ingrese monto $ </label>
-            <input type="number" id="pesos" v-model.number="pesos">
-        </div>
-        <div>
-            <label for="cotizacion">Valor del Dólar en $ </label>
-            <input type="number" id="cotizacion" v-model.number="cotizacion">
-        </div>
-        <div>
-            <label for="resultado">Valor convertido USD {{ resultado }}</label>
-        </div>
-        <hr>
+        <label for="cantidad">Monto en Pesos $ </label>
+        <input type="number" v-model.number="pesos" id="cantidad"><br>
+
+        <label for="cotizacion">Cotización del Dólar $ </label>
+        <input type="number" v-model.number="cotizacion" id="cotizacion" :disabled="autoActualizar">
+
+        <label for="autoActualizar"> Actualizar Automáticamente:</label>
+        <input type="checkbox" v-model="autoActualizar" id="autoActualizar">
+
+        <p v-if="autoActualizar">Última Actualización: {{ ultimaActualizacion }}</p>
+
+        <p v-if="cotizacion && pesos">Cantidad en Dólares US$ {{ convertirADolares() }}</p>
 
         <p>Respuestas:</p>
         <ol>
@@ -25,24 +25,72 @@
         <hr>
 
         <footer>Julian Taiter - Curso: 21E</footer>
+
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
     name: 'App',
     data() {
-    return {
-      pesos: null,
-      cotizacion: null
-    };
-  },
-  computed: {
-    resultado() {
-      if (this.cotizacion === 0) return 0;
-      return (this.pesos / this.cotizacion).toFixed(2);
-    }
-  }};
-</script>
+        return {
+            pesos: null,
+            cotizacion: null,
+            autoActualizar: false,
+            ultimaActualizacion: null
+        };
+    },
+    watch: {
+        actualizando(newValue) {
+            if (newValue) {
+                this.actualizarCotizacionAutomaticamente();
+            } else {
+                this.detenerActualizacionAutomatica();
+            }
+        }
+    },
+    methods: {
+        async obtenerCotizacionYActualizar() {
+            try {
+                const response = await axios.get('https://api.bluelytics.com.ar/v2/latest')
+                this.cotizacion = response.data.blue.value_sell
+                this.ultimoUpdate = Date.now()
+            } catch (error) {
+                console.error('Error al obtener la cotización y actualizar:', error);
+            }
+        },
+        async actualizarCotizacionAutomaticamente() {
+            await this.obtenerCotizacionYActualizar();
+            this.intervalo = setInterval(async () => {
+                await this.obtenerCotizacionYActualizar();
+            }, 2000);
+        },
+        detenerActualizacionAutomatica() {
+            clearInterval(this.intervalo);
+            this.intervalo = null;
+        },
+        convertirADolares() {
+            return (this.pesos / this.cotizacion).toFixed(2);
+        },
+        activarActualizacionAutomatica() {
+            if (this.autoActualizar) {
+                this.intervalId = setInterval(this.obtenerCotizacion, 120000);
+            } else {
+                clearInterval(this.intervalId);
+            }
+        }
+    },
+    created() {
+        this.obtenerCotizacionYActualizar();
 
-<style scoped></style>
+        setInterval(() => {
+            if (this.autoActualizar) {
+                this.obtenerCotizacionYActualizar();
+            }
+        }, 2000);
+    }
+};
+</script>
